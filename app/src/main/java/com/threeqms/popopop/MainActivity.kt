27b315a989fb13e7dd77392546e24701585cec80
Alexhwa <ah.alswad@gmail.com>
2022-10-28 11:04:15 -0500

    Merging

--- app/src/main/java/com/threeqms/popopop/MainActivity.kt
+++ app/src/main/java/com/threeqms/popopop/MainActivity.kt
@@ -1,35 +1,35 @@
 package com.threeqms.popopop
 
-import android.content.Intent
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import com.threeqms.popopop.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val b1 = findViewById<View>(R.id.start_button)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        b1.setOnClickListener {
-            val game = Intent(this@MainActivity, Gameplay::class.java)
-            startActivity(game)
-        }
+        val vi = applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = vi.inflate(R.layout.fragment_kernel, null)
 
+        findViewById<FrameLayout>(R.id.frameLayout).addView(view)
 
+//        var v : View = View.inflate(applicationContext, R.layout.fragment_kernel, findViewById(R.id.frameLayout))
+        view.layoutParams.width = 300
+        view.layoutParams.height = 300
+        view.x = 100f
+        view.y = 100f
     }
 
-    /**
-     * Enables back button support. Simply navigates one element up on the stack.
-     */
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
 }
