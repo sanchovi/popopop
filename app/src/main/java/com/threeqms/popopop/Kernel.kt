d23504f673a1600db1b4690d36e8a534683d31db
Alexhwa <ah.alswad@gmail.com>
2022-10-28 18:03:46 -0500

    physics simulation and wall collision

--- app/src/main/java/com/threeqms/popopop/Kernel.kt
+++ app/src/main/java/com/threeqms/popopop/Kernel.kt
@@ -2,21 +2,22 @@ package com.threeqms.popopop
 
 import android.view.View
 import kotlin.math.roundToInt
+import com.threeqms.popopop.KernelSimulator
 
-class Kernel(pos : Vector2, r : Float, v : View) {
+class Kernel(pos : Vector2, r : Float, v : View, startVel : Vector2, startAngVel : Float) {
 
     companion object {
-        val GRAVITY = 9.81f
+        val GRAVITY = 100f
         val ELASTICITY = 0.9f
     }
     
     val view : View = v
 
     val position : Vector2 = pos
-    val rotation : Float = 0f
+    var rotation : Float = 0f
     val radius : Float = r
-    val velocity : Vector2 = Vector2()
-    val angularVelocity : Float = 0f
+    val velocity : Vector2 = startVel
+    val angularVelocity : Float = startAngVel
     val popProgress : Float = 0f
     val mass : Float = 1f
 
@@ -26,10 +27,51 @@ class Kernel(pos : Vector2, r : Float, v : View) {
 
     fun updateView()
     {
-        view.layoutParams.width = radius.roundToInt()
-        view.layoutParams.height = radius.roundToInt()
-        view.x = (position.x - radius / 2f)
-        view.y = (position.y - radius / 2f)
+        view.layoutParams.width = (2 * radius).roundToInt()
+        view.layoutParams.height = (2 * radius).roundToInt()
+        view.x = (position.x - radius)
+        view.y = (position.y - radius)
         view.rotation = rotation
+    }
+
+    fun update(ks: KernelSimulator, dt : Float){
+        velocity += Vector2(0f, GRAVITY) * dt
+        val newPos : Vector2 = position + velocity * dt
+
+        var yTime : Float = 1f
+        //Find the percentage of the velocity it takes to collide with the wall. Can also be negative. (Botton)
+        if(velocity.y > 0){
+            yTime = (ks.getMaxBounds().y - (position.y + radius)) / (newPos.y - position.y)
+        }
+        //(Top)
+        if(velocity.y < 0){
+            yTime = (ks.getMinBounds().y - (position.y - radius)) / (newPos.y - position.y)
+        }
+
+        var xTime : Float = 1f
+        //Find the percentage of the velocity it takes to collide with the wall. Can also be negative. (Right)
+        if(velocity.x > 0){
+            xTime = (ks.getMaxBounds().x - (position.x + radius)) / (newPos.x - position.x)
+        }
+        //(Left)
+        if(velocity.x < 0){
+            xTime = (ks.getMinBounds().x - (position.x - radius)) / (newPos.x - position.x)
+        }
+        //Use percentage of velocity to place next to wall but not past it
+        if(yTime < 1 || xTime < 1){
+            position += Math.min(yTime, xTime) * velocity * dt
+            if(yTime == Math.min(yTime, xTime)){
+                velocity *= Vector2(1f, -1f) * ELASTICITY
+
+            }
+            if(xTime == Math.min(yTime, xTime)){
+                velocity *= Vector2(-1f, 1f) * ELASTICITY
+            }
+        }
+        else{
+            position +=  velocity * dt
+        }
+
+        rotation += angularVelocity * dt
     }
 }
