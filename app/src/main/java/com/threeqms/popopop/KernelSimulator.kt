005fb79b894e6180ea27852ca443d0312464a4b5
Alexhwa <ah.alswad@gmail.com>
2022-10-28 18:09:51 -0500

    Update KernelSimulator.kt

--- app/src/main/java/com/threeqms/popopop/KernelSimulator.kt
+++ app/src/main/java/com/threeqms/popopop/KernelSimulator.kt
@@ -1,14 +1,34 @@
 package com.threeqms.popopop
 
+import android.util.DisplayMetrics
 import android.view.View
+import androidx.annotation.FloatRange
 import java.util.*
+import android.app.Activity
 
-class KernelSimulator {
+
+class KernelSimulator(boundsMin: Vector2, boundsMax: Vector2){
     val kernels: MutableList<Kernel?> = mutableListOf()
     val freeIds: Queue<Int> = LinkedList<Int>(listOf())
+    private val minBounds : Vector2 = boundsMin
+    private  val maxBounds : Vector2 = boundsMax
+    private val boundsSize : Vector2 =  maxBounds - minBounds
 
-    fun addKernel(kernelView: View): Kernel {
-        val kernel = Kernel(Vector2(), 10f, kernelView)
+
+    fun addKernel(kernelView: View, radiusRange : Vector2 = Vector2(80f, 100f)): Kernel {
+        val randomX = minBounds.x + Math.random().toFloat() * boundsSize.x
+        val randomY = minBounds.y + Math.random().toFloat() * boundsSize.y / 2
+
+        val randomRadius = radiusRange.x + Math.random().toFloat() * (radiusRange.y - radiusRange.x)
+
+        //TODO: add global constants for minStart speed and maxStartSpeed
+        val randomStartSpeed = 100f + Math.random().toFloat() * 200f
+        val randomDirection = Vector2(1f, 0f).rotated(Math.random().toFloat() * 360)
+        val randomStartVelocity = randomDirection * randomStartSpeed
+
+        val randomAngular = -45f + Math.random().toFloat() * 90f
+        val kernel = Kernel(Vector2(randomX, randomY), randomRadius, kernelView, randomStartVelocity, randomAngular)
+
 
         if (!freeIds.isEmpty()) {
             val id: Int = freeIds.poll()
@@ -36,13 +56,20 @@ class KernelSimulator {
                 continue
             var kernel: Kernel = kernelOpt!!
 
-            kernel.velocity += Vector2(0f, Kernel.GRAVITY) * dt
-            kernel.position += kernel.velocity * dt
+            kernelOpt.update(this, dt)
         }
 
         // TODO update the positions and rotations of the views
         for (kernelOpt in kernels) {
             kernelOpt?.updateView()
         }
+    }
+
+    fun getMaxBounds(): Vector2 {
+        return maxBounds
+    }
+
+    fun getMinBounds(): Vector2 {
+        return minBounds
     }
 }
