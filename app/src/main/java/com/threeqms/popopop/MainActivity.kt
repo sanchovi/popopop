2350c63f848edcd18df36f20e1040bff6272eaf8
sanchovi <sanchez.michelle@utexas.edu>
2022-10-28 13:41:38 -0500

    Layout and .kt files for main page, gameplay, and shop navigation

--- app/src/main/java/com/threeqms/popopop/MainActivity.kt
+++ app/src/main/java/com/threeqms/popopop/MainActivity.kt
@@ -1,72 +1,35 @@
 package com.threeqms.popopop
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.threeqms.popopop.databinding.ActivityMainBinding
-import kotlinx.coroutines.*
+import androidx.navigation.NavController
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var inflater: LayoutInflater
-    private lateinit var container: FrameLayout
-    private lateinit var simulator: KernelSimulator
-    private var isRunning: Boolean = false
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        inflater = applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        binding = ActivityMainBinding.inflate(inflater)
-        setContentView(binding.root)
+        val b1 = findViewById<View>(R.id.start_button)
 
-        container = findViewById<FrameLayout>(R.id.frameLayout)
+        b1.setOnClickListener {
+            val game = Intent(this@MainActivity, Gameplay::class.java)
+            startActivity(game)
+        }
+
 
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        container.post(Runnable{
-            val minPoint = Vector2(container.x, container.y)
-            val maxPoint = Vector2(container.x + container.width, container.y + container.height)
-            simulator = KernelSimulator(minPoint, maxPoint)
-            //for(i in 1..25){
-                simulator.addKernel(createKernelView())
-            //}
-
-            var previousMillis: Long = System.currentTimeMillis()
-            GlobalScope.launch {
-                isRunning = true
-
-                while (isRunning) {
-                    var currentMillis: Long = System.currentTimeMillis()
-                    var dtMillis: Long = currentMillis - previousMillis
-                    if (dtMillis > 67)
-                        dtMillis = 67
-                    var previousMillis = currentMillis
-
-                    simulator.simulate(dtMillis / 1000.0f)
-
-                    currentMillis = System.currentTimeMillis()
-                    var desiredWaitTime: Long = 16 - (currentMillis - previousMillis)
-                    if (desiredWaitTime > 0)
-                        delay(desiredWaitTime)
-                }
-            }
-        })
+    /**
+     * Enables back button support. Simply navigates one element up on the stack.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-    fun createKernelView() : View
-    {
-        val view: View = inflater.inflate(R.layout.fragment_kernel, null)
-        container.addView(view)
-        return view
-    }
-
 }
