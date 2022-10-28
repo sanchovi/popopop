ec55fe7a5b946bc70c9ebb4dd3e9896529633946
Alexhwa <ah.alswad@gmail.com>
2022-10-28 11:04:15 -0500

    Merging

---
+++ app/src/main/java/com/threeqms/popopop/Kernel.kt
@@ -0,0 +1,18 @@
+package com.threeqms.popopop
+
+import androidx.fragment.app.Fragment
+
+class KernelFragment(pos : Vector2, r : Float) : Fragment() {
+    val position : Vector2 = pos
+    val rotation : Float = 0f
+    val radius : Float = r
+    val velocity : Vector2 = Vector2()
+    val angularVelocity : Float = 0f
+    val popProgress : Float = 0f
+    val mass : Float = 1f
+
+    companion object {
+        val gravity = 9.81f
+        val elasticity = 0.9f
+    }
+}
--- app/src/main/java/com/threeqms/popopop/MainActivity.kt
+++ app/src/main/java/com/threeqms/popopop/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.threeqms.popopop
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.threeqms.popopop.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val vi = applicationContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = vi.inflate(R.layout.fragment_kernel, null)
+
+        findViewById<FrameLayout>(R.id.frameLayout).addView(view)
+
+//        var v : View = View.inflate(applicationContext, R.layout.fragment_kernel, findViewById(R.id.frameLayout))
+        view.layoutParams.width = 300
+        view.layoutParams.height = 300
+        view.x = 100f
+        view.y = 100f
     }
+
 }
