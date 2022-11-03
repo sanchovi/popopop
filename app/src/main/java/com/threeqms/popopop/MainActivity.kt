a9dfc5470feaf97ab485c4af72a0829d4647f877
sanchovi <sanchez.michelle@utexas.edu>
2022-11-03 11:17:31 -0500

    Committing to save MainActivity changes

--- app/src/main/java/com/threeqms/popopop/MainActivity.kt
+++ app/src/main/java/com/threeqms/popopop/MainActivity.kt
@@ -1,67 +1,35 @@
 package com.threeqms.popopop
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import com.threeqms.popopop.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
-
-    private lateinit var sensorManager:SensorManager
-    private lateinit var accelerometer :Sensor
-    private lateinit var gyroscope :Sensor
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var inflater: LayoutInflater
+    private lateinit var container: FrameLayout
+    private lateinit var simulator: KernelSimulator
+    private var isRunning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gameplay)
 
-        val b1 = findViewById<View>(R.id.start_button)
+        val b1 = findViewById<View>(R.id.shop_button)
+
+        b1.setOnClickListener {
+            val shop = Intent(this@MainActivity, Gameplay::class.java)
+            startActivity(shop)
+        }
+
 
     }
-
-    override fun onStart() {
-        super.onStart()
-
-        container.post(Runnable{
-            val minPoint = Vector2(container.x, container.y)
-            val maxPoint = Vector2(container.x + container.width, container.y + container.height)
-            simulator = KernelSimulator(minPoint, maxPoint)
-            for(i in 1..25){
-                simulator.addKernel(createKernelView())
-            }
-
-            var previousMillis: Long = System.currentTimeMillis()
-            GlobalScope.launch {
-                isRunning = true
-
-                while (isRunning) {
-                    var currentMillis: Long = System.currentTimeMillis()
-                    var dtMillis: Long = currentMillis - previousMillis
-                    if (dtMillis > 67)
-                        dtMillis = 67
-                    var previousMillis = currentMillis
-
-                    simulator.simulate(dtMillis / 1000.0f)
-
-                    currentMillis = System.currentTimeMillis()
-                    var desiredWaitTime: Long = 16 - (currentMillis - previousMillis)
-                    if (desiredWaitTime > 0)
-                        delay(desiredWaitTime)
-                }
-            }
-        })
-    }
-
-    fun createKernelView() : View
-    {
-        val view: View = inflater.inflate(R.layout.fragment_kernel, null)
-        container.addView(view)
-        return view
-    }
-
 }
